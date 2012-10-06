7276b0131b0b75f4ab74a9243103b4cfd7ed812b
Richard Harrington <richard.harrington@ciltrax.com>
2012-10-06 15:52:36 -0400

    Added default updates

--- src/Taboo/MainBundle/Controller/DefaultController.php
+++ src/Taboo/MainBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('TabooMainBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('TabooMainBundle:Default:index.html.twig', array('name' => $name, 'name2' => 'hi'));
     }
 }
