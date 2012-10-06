bfe99cb58545ade5ecbe367ed3f99e909016ca98
unknown <richard.harrington@rharringtonT420.adnetik.local>
2012-10-06 15:37:55 -0400

    added gitignore

---
+++ src/Taboo/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Taboo\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('TabooMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Taboo/MainBundle/TabooMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Taboo\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TabooMainBundle extends Bundle
+{
+}
