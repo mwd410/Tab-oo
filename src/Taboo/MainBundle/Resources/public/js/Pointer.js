f38c21b2f1fc7fd177802df8064cdb794cf006a4
Matt Deady <mwd410@comcast.net>
2012-10-20 20:23:23 -0400

    wtf

--- src/Taboo/MainBundle/Resources/public/js/Pointer.js
+++ src/Taboo/MainBundle/Resources/public/js/Pointer.js
@@ -32,9 +32,30 @@ Taboo.Pointer = function(config) {
         height : Taboo.grid,
         fill : 'white'
     });
+
+    var stem = new Kinetic.Line({
+        x : 0,
+        points : [0, 0, 0, 2.5 * Taboo.grid],
+        stroke : "red",
+        strokeWidth: 1
+    });
+    this.pieces.push(stem);
+    for (var i = 0; i < 3; ++i) {
+        var tail = new Kinetic.Path({
+            data : "M426 1434q4 -41 13.5 -75t19.5 -63q14 -51 45 -94l35 -43l49 -59l90 -105q102 -133 102 -278q0 -35 -6 -77t-24 -87q-15 -35 -24 -58.5t-26 -52.5l-30 -2l35 90l12 37.5t10 40.5l6 56l2 24v41l-8 33l-16 49q-21 63 -70 115q-45 57 -98 84q-29 16 -60.5 26.5t-56.5 10.5",// v-814q0"// -63"// -45"// -120q-27"// -45 -102 -80q-64 -33 -134 -33q-63 0 -102 33q-43 30 -43 92q0 31"// 9 60.5t32 56.5q20 24 46 46.5t60 39.5q68 30 123"// 30q88 0 121 -32v733v375h35z"
+            fill: "red",
+            scale: [0.022,0.022],
+            y : -i * Taboo.grid / 2,
+            x :  -Taboo.grid * 3/4//this.stem.getX()-Taboo.Global.grid*1/12//
+        });
+        this.pieces.push(tail);
+    }
     this.layer.setPosition(20,20);
     this.layer.add(this.halo);
     this.layer.add(this.number);
+    for (i = 0; i < this.pieces.length; ++i) {
+        this.layer.add(this.pieces[i]);
+    }
     this.stateless.push(
         "pieces"
         );
