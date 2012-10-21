c4f5f5c90fefbfa723a50fade764fb38433622c4
Richard Harrington <rwharrington87@gmail.com>
2012-10-20 21:42:24 -0400

    started user registration

--- src/Taboo/MainBundle/Controller/PersonController.php
+++ src/Taboo/MainBundle/Controller/PersonController.php
@@ -3,12 +3,17 @@
 namespace Taboo\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PersonController extends Controller {
     
-    public function createAction() {
+    public function createAction(Request $request) {
         
-        
+        if ($request->isMethod('POST')) {
+            
+            return new Response('Hello ', 200);             
+        }
         
         return $this->render('TabooMainBundle:Person:create.html.twig');
     }
